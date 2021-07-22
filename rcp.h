df1e449b515ad49889abf047f690a17777b872da
smplusr <83000221+smplusr@users.noreply.github.com>
2021-07-22 19:06:33 +0200

    Update rcp.h
    Updating preprocessors names and definitions.

--- rcp.h
+++ rcp.h
@@ -2,16 +2,17 @@
 #include<cstdlib>
 #include<string>
 #include<fstream>
-#define COMPILE(x)\
+#define RCP_COMPILE(x)\
 	ifs.open(x);\
 	tmp_string.assign((std::istreambuf_iterator<char>(ifs)),(std::istreambuf_iterator<char>()));\
 		system(tmp_string.c_str());\
 		ifs.close();
+#endif
 
-#else
+#ifndef __cplusplus
 #include<stdio.h>
 #include<stdlib.h>
-#define COMPILE(x)\
+#define RCP_COMPILE(x)\
 	if(fptr==NULL) return;\
 	fptr=fopen(x, "r");\
 	while(getline(&line,&len,fptr)!=EOF) system(line);\
@@ -19,20 +20,20 @@
 
 #endif
 
-#if(AUTO_RECOMPILE!=0)
+#if(RCP_AUTO_RECOMPILE!=0)
 __attribute((destructor)) void END_PROG(){
 	#ifdef __cplusplus 
 	std::string tmp_string;std::ifstream ifs;
 	#else
 	FILE *fptr;char *line=NULL;size_t len=0;
 	#endif
-	#ifdef SOURCE_PATH
-		#ifdef HEADER_PATH
-			#if(PRECOMPILE_HEADERS!=0)
-				COMPILE(HEADER_PATH);
+	#ifdef RCP_SOURCE_PATH
+		#ifdef RCP_HEADER_PATH
+			#if(RCP_PRECOMPILE_HEADERS!=0)
+				RCP_COMPILE(RCP_HEADER_PATH);
 			#endif
 		#endif
-		COMPILE(SOURCE_PATH);
+		RCP_COMPILE(RCP_SOURCE_PATH);
 	#endif	
 }
 #endif
